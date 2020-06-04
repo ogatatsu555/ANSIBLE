ad8b344cb5a9862665c24119a5ff9f408d8c8c4a
TatsuyaOgawa <ogawa@itpm-gk.com>
2020-06-04 17:55:04 +0900

    20200604_mem_web

---
+++ roles/abc.sh
@@ -0,0 +1,13 @@
+./apache_mem_web_tepco/templates/robots.txt
+./apache_mem_web_tohoku/templates/robots.txt
+./apache_mem_web_energia/templates/robots.txt
+./apache_mem_web_rikuden2/templates/robots.txt
+./apache_mem_web_yonden/templates/robots.txt
+./apache_mem_web_chuden/templates/robots.txt
+./apache_mem_web_02_tepco/templates/robots.txt
+./apache_mem_web_02_tohoku/templates/robots.txt
+./apache_mem_web_02_energia/templates/robots.txt
+./apache_mem_web_02_rikuden2/templates/robots.txt
+./apache_mem_web_02_yonden/templates/robots.txt
+./apache_mem_web_02_chuden/templates/robots.txt
+./apache_mem_web_test_tepco/templates/robots.txt
